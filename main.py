36bfec6cd74fa4b2c82afef59b703d4fead4bc81
Mayorov Alexander <mayorov.shurik@mail.ru>
2023-10-24 23:59:40 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+from pathlib import Path
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
+
+if __name__ == "__main__":
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+    qml_file = Path(__file__).parent / ''
+    engine.load(qml_file)
+    if not engine.rootObjects():
+        sys.exit(-1)
+    exit_code = app.exec()
+    del engine
+    sys.exit(exit_code)
