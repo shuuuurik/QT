94aeb7e801d8a55c9ce7f8fff3c67beb54444a4b
Mayorov Alexander <mayorov.shurik@mail.ru>
2023-10-29 19:28:09 +0300

    Add Examples folder and move all previous examples into it

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from PySide6.QtQml import QQmlApplicationEngine
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
-    qml_file = Path(__file__).parent / 'qqc2_example/main.qml'
+    qml_file = Path(__file__).parent / 'Examples/qqc2_example/main.qml'
     engine.load(qml_file)
     if not engine.rootObjects():
         sys.exit(-1)
