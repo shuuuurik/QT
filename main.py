28858b5e874ce04a14c11858679a88653ef00b69
Mayorov Alexander <mayorov.shurik@mail.ru>
2023-12-06 22:11:21 +0300

    Практика 9: Задание по работе с WorkerScript

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from PySide6.QtQml import QQmlApplicationEngine
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
-    qml_file = Path(__file__).parent / 'Examples/threads/main.qml'
+    qml_file = Path(__file__).parent / 'Lab09\ex1.qml'
     engine.load(qml_file)
     if not engine.rootObjects():
         sys.exit(-1)
